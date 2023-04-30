9e794c8107531db7206310ac8b78f4b5f7794dda
juni0317 <97174617+juni0317@users.noreply.github.com>
2023-05-01 00:16:10 +0900

    web page

---
+++ script.js
@@ -0,0 +1,18 @@
+const electronContainer = document.querySelector(".electron-container");
+const electrons = document.querySelectorAll(".electron");
+const nucleus = document.querySelector(".nucleus");
+
+let rotation = 0;
+
+function animateElectrons() {
+  rotation += 1;
+  electrons.forEach((electron, index) => {
+    const angle = rotation + index * (360 / electrons.length);
+    const x = Math.cos(angle * (Math.PI / 180)) * 200;
+    const y = Math.sin(angle * (Math.PI / 180)) * 200;
+    electron.style.transform = `translate(${x}px, ${y}px)`;
+  });
+  requestAnimationFrame(animateElectrons);
+}
+
+animateElectrons();
